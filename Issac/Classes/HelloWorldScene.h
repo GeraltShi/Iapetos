35e0a0f798f709dd44b190884217d38b513ceab5
geralt47 <paladin.heleth@gmail.com>
2018-03-06 19:05:10 +0800

    add keyboard support

--- Issac/Classes/HelloWorldScene.h
+++ Issac/Classes/HelloWorldScene.h
@@ -10,11 +10,20 @@ public:
 
     virtual bool init();
     
+    cocos2d::Sprite *sprite;
+    
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
+    void update(float delta);
+    
+    bool isKeyPressed(cocos2d::EventKeyboard::KeyCode);
+    void keyPressedDuration(cocos2d::EventKeyboard::KeyCode);
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+    
+private:
+    std::map<cocos2d::EventKeyboard::KeyCode, bool> keys;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
