db7739d7ea51417fcb57f80d92e2556f8a3c8592
geralt47 <paladin.heleth@gmail.com>
2018-06-12 13:43:47 +0800

    增加结束界面和按键响应

--- Issac/Classes/Model/RoomSceneModel.hpp
+++ Issac/Classes/Model/RoomSceneModel.hpp
@@ -16,8 +16,9 @@ public:
         paused_menu_cursor = (paused_menu_cursor+wd+3)%3;
     }
     bool bomb = false;
-    bool paused = false;
+    int game_stat = 0;// 游戏状态控制，0为运行，1为暂停，2为结束
     int paused_menu_generated_flag = 0;
     int paused_menu_cursor = 1;
+    int dead_menu_generated_flag = 0;
 };
 #endif /* RoomSceneModel_hpp */
