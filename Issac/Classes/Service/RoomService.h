cf88d0a5b15b308f5b59213a2b3f0680283a40b6
蔡俊哲 <godhunt@126.com>
2018-06-15 22:59:22 +0800

    迷宫做完了，重写了RoomService的init函数，原函数被注释了 迷宫大小：maze_size 10（可以调小了试试）

--- Issac/Classes/Service/RoomService.h
+++ Issac/Classes/Service/RoomService.h
@@ -9,6 +9,13 @@
 #include "Model/RoomSceneModel.hpp"
 #include "cocos2d.h"
 
+#define maze_size 10
+#define MYDOWN 0
+#define MYRIGHT 1
+#define MYLEFT 2
+#define MYUP 3
+#define MAZEWALL 0
+
 USING_NS_CC;
 using namespace std;
 
@@ -49,12 +56,17 @@ public:
     //0.0~1.0
     float get_real_music_volume() const;
 
+	//创造迷宫,产生每个房间的roomID
+	void createMaze();
+	//根据MAZE做init
+
     CC_SYNTHESIZE(int, music_volume, MusicVolume)
     CC_SYNTHESIZE(int, sfx_volume, SFXVolume)
         //0 ~ 10  --> 0 ~ 255
     CC_SYNTHESIZE(int, mini_opacity, MiniOpacity)
     CC_SYNTHESIZE(bool, win_, Win)
     CC_SYNTHESIZE(int, play_bg_music_id, PlayBgMusicId)
+
 
 private:
     static RoomService *inst_;
@@ -72,8 +84,16 @@ private:
     void build_vm_from_room(Room room_m);
 
     RoomSceneModel saved_room_;
+
+	//迷宫地图,产生每个房间的roomID
+	int m[maze_size][maze_size];
+	//判断点在地图中
+	bool inmaze(int x, int y) {
+		return (x >= 0 && x < maze_size && y >= 0 && y < maze_size);
+	}
 };
 
+int randomRoom();
 
 #endif // _ROOMSERVICE_H_
 
