b6815fc301e8c00395b9dc301a160eaac760d54d
geralt47 <paladin.heleth@gmail.com>
2018-06-15 23:52:51 +0800

    添加 collectible 贴图

--- Issac/Classes/Character/Collectable.cpp
+++ Issac/Classes/Character/Collectable.cpp
@@ -53,31 +53,31 @@ bool Collectable::init(int collectableType)
 		break;
 	//下面的贴图都是不对的,全用蓝心代替
 	case(3):	//加攻击3,扣1颗心的血，减慢移动速度
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_013_thevirus.png"), Rect(0, 0, 32, 32));
 		attack = 3;
 		health = -2;
 		moveSpeed = -20;
 		break;
 	case(4):	//加攻击1
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_346_asnack.png"), Rect(0, 0, 32, 32));
 		attack = 1;
 		break;
 	case(5):	//减攻击0.7，射程几乎无限
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_076_xrayvision.png"), Rect(0, 0, 32, 32));
 		attack = -0.3;
 		tearExistTime = 999;
 		break;
 	case(6):	//加攻击4，射程减半
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_230_abaddon.png"), Rect(0, 0, 32, 32));
 		attack = 4;
 		enhalfTearDis = true;
 		break;
 	case(7):	//子弹现在反弹了
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_194_magic8ball.png"), Rect(0, 0, 32, 32));
 		enBounce = true;
 		break;
-	case(8):	//人能够飞了
-		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/pick ups/pickup_001_heart.png"), Rect(0, 32, 32, 32));
+	case(8):	//人能够飞了, 最好能换成吸血鬼皮肤
+		temp_collectableImg = Sprite::createWithTexture(Director::getInstance()->getTextureCache()->addImage("res/gfx/items/collectibles/collectibles_062_charmofthevampire.png"), Rect(0, 0, 32, 32));
 		enFly = true;
 		break;
 	}
